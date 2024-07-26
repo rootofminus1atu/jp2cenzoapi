e1de66e3eb7232b1a902ac30514337b0dd01b9c6
RootOfMinus1ATU <s00240122@atu.ie>
2024-07-26 18:04:54 +0100

    test commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    let app = Router::new().route("/", get(|| async { 
+        tracing::info!("hello sent!");
+        "Hello, World!" 
+    }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("listening");
+    axum::serve(listener, app).await.unwrap();
+}
